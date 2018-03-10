8d71a47aaa38eb08f84f8877c56c890d191ecdb0
Avaer Kazmer <a@modules.io>
2018-03-09 21:12:16 -0800

    Bugfix index.js server

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const path = require('path');
 const http = require('http');
 const express = require('express');
 
+const site = express.static(__dirname);
+
 const app = express();
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', (req, res, next) => {
+  if (req.path === '/exokit') {
+    res.type('text/html');
+  }
+
+  site(req, res, next);
+});
 
 http.createServer(app)
   .listen(8080);
