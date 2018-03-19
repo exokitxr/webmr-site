a421387f03ce43cb5c5111ba2166ccb1e9993017
Avaer Kazmer <a@modules.io>
2018-03-19 09:23:40 -0700

    Simplify index.js

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 const http = require('http');
 const express = require('express');
 
-const site = express.static(__dirname);
-
 const app = express();
-app.use('/', (req, res, next) => {
-  if (req.path === '/exokit') {
-    res.type('text/html');
-  }
-
-  site(req, res, next);
-});
+app.use(express.static(__dirname));
 
 http.createServer(app)
   .listen(8080);
