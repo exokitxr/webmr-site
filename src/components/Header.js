d23422c8903021fe33f4feb6cf3435597d3ceb50
Avaer Kazmer <a@modules.io>
2018-03-19 09:48:57 -0700

    Add webmr publish instructions to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,13 @@ class Header extends React.Component {
   }
 
   render() {
-    return <header style={{display: 'flex', height: '80px', padding: '0 20px', backgroundImage: 'linear-gradient(to right, #dce35b, #45b649)', color: '#FFF', alignItems: 'center'}}>
-      <div style={{fontFamily: '"Press Start 2P"', fontSize: '20px', textShadow: '-2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000'}}>WEBMR</div>
+    return <header style={{display: 'flex', fontFamily: 'Source Code Pro', flexDirection: 'column'}}>
+      <div style={{display: 'flex', height: '80px', padding: '0 20px', backgroundImage: 'linear-gradient(to right, #dce35b, #45b649)', color: '#FFF', alignItems: 'center'}}>
+        <div style={{fontFamily: '"Press Start 2P"', fontSize: '20px', textShadow: '-2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000'}}>WEBMR</div>
+      </div>
+      <div style={{display: 'flex', height: '40px', padding: '0 20px', backgroundColor: '#222', color: '#FFF', fontSize: '13px', fontWeight: 600, whiteSpace: 'pre', alignItems: 'center'}}>
+        webmr publish /my-node-module <span style={{color: '#666'}}># publish to webmr.io</span>
+      </div>
     </header>;
   }
 }
