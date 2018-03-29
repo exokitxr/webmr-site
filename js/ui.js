d4383d5d7bb167e3192d26399073402af1b38021
Emily Kolar <emkolardev@gmail.com>
2018-03-29 02:30:09 -0500

    second round; lots of text content added

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,65 @@
 (function() {
+  function prepScrolls() {
+    window.addEventListener('scroll', function(evt) {
+      const logos = document.getElementById('logos');
+      const logoItems = document.querySelectorAll('.platform');
+      // const top = logos.getBoundingClientRect().top;
+      const height = window.innerHeight;
+      logoItems.forEach(function(item) {
+        if (item.getBoundingClientRect().top < height / 2) {
+          if (!item.classList.contains('animated')) {
+            item.classList.add('animated');
+          }
+        } else {
+          if (item.classList.contains('animated')) {
+            item.classList.remove('animated');
+          }
+        }
+      })
+      // if (top < height / 2) {
+      //   if (!logoItems[0].classList.contains('animated')) {
+      //     logoItems.forEach(function(item) {
+      //       item.classList.add('animated');
+      //     });
+      //   }
+      // } else {
+      //   if (logoItems[0].classList.contains('animated')) {
+      //     logoItems.forEach(function(item) {
+      //       item.classList.remove('animated');
+      //     });
+      //   }
+      // }
+    });
+  }
+
+  function selectText(elementId) {
+    if (document.selection) {
+      var range = document.body.createTextRange();
+      range.moveToElementText(document.getElementById(elementId));
+      range.select();
+    } else if (window.getSelection) {
+      var range = document.createRange();
+      range.selectNode(document.getElementById(elementId));
+      window.getSelection().removeAllRanges();
+      window.getSelection().addRange(range);
+    }
+  }
+
   function addListeners() {
-    var teaser = $('#teaser');
-    var howItWorks = $('#what-is');
-    var exploded = $('#exploded-graphic');
-    var learnMore = $('#learn-more');
+    const codeInstall = document.getElementById('code-install');
+    const codeRun = document.getElementById('code-run');
+    const teaser = $('#teaser');
+    const howItWorks = $('#what-is');
+    const exploded = $('#exploded-graphic');
+    const learnMore = $('#learn-more');
+
+    codeInstall.addEventListener('click', function(evt) {
+      selectText(evt.target.id);
+    });
+
+    codeRun.addEventListener('click', function(evt) {
+      selectText(evt.target.id);
+    });
 
     teaser.on('click', function() {
       $('html, body').animate({
@@ -42,5 +98,6 @@
 
   window.addEventListener('load', function () {
     addListeners();
+    prepScrolls();
   });
 })();
